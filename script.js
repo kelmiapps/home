876fe3e459d0f31b2d90161cb1e4deffce1f9894
Antti Joutsenniemi <tinpurittna@gmail.com>
2023-02-23 18:47:05 +0200

    updated versions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Modal Image Gallery
 /*
-function onClick(element) {
+function fullscreen(element) {
     document.getElementById("img01").src = element.src;
     document.getElementById("modal01").style.display = "block";
     var captionText = document.getElementById("caption");
